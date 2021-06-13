7e6232e308c1fc8c63dc132ff72d1f3b6355b9a7
le.trong <le.trong>
2021-06-13 18:16:45 -0400

    Updated lamda handler get params

--- api/lambda/main.go
+++ api/lambda/main.go
@@ -5,7 +5,12 @@ import (
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/uuid"
+	corid "github.com/hthl85/aws-lambda-corid"
 	logger "github.com/hthl85/aws-lambda-logger"
+	"github.com/hthl85/aws-tiprank-norm-dividend/config"
+	"github.com/hthl85/aws-tiprank-norm-dividend/infrastructure/repositories/mongodb/repos"
+	tiprankdividends "github.com/hthl85/aws-tiprank-norm-dividend/usecase/tiprank-dividends"
 )
 
 type TipRankDividendRequest struct {
@@ -13,6 +18,14 @@ type TipRankDividendRequest struct {
 }
 
 func main() {
+	lambda.Start(lambdaHandler)
+}
+
+func lambdaHandler(ctx context.Context, req TipRankDividendRequest) {
+	log.Println("lambda handler is called")
+
+	appConf := config.AppConf
+
 	// create new logger
 	zap, err := logger.NewZapLogger()
 	if err != nil {
@@ -20,10 +33,19 @@ func main() {
 	}
 	defer zap.Close()
 
-	lambda.Start(lambdaHandler)
-}
+	// create new repository
+	repo, err := repos.NewTipRankDividendMongo(nil, zap, &appConf.Mongo)
+	if err != nil {
+		log.Fatalf("create TipRank dividend mongo repo failed: %v\n", err)
+	}
+	defer repo.Close()
 
-func lambdaHandler(ctx context.Context, req TipRankDividendRequest) {
-	log.Println("lambda handler is called")
-	log.Println("request", req.Tickers)
+	// create new service
+	tiprankService := tiprankdividends.NewService(repo, zap)
+
+	// try correlation context
+	id, _ := uuid.NewRandom()
+	coridCtx := corid.NewContext(ctx, id)
+	dividends, err := tiprankService.FindTipRankDividends(coridCtx, req.Tickers)
+	zap.Info(coridCtx, "TipRank Dividends", "dividends", dividends)
 }
